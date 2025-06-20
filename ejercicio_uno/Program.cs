73c05f3290bf4e564c8b37e7422b707f15b9673c
Gian <77172995+giansp@users.noreply.github.com>
2025-06-19 21:00:42 -0300

    Add files via upload

---
+++ ejercicio_uno/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Promedio20Num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double i,acu,num,promedio;
+            promedio = 0;
+            acu = 0;
+            i = 1;
+            for (i=1; i<=20;i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                num = Convert.ToDouble(Console.ReadLine());
+                acu = acu + num;
+                promedio=acu /i;
+                Console.WriteLine("El promedio para " + i + " numeros es: " + promedio);
+            }
+            Console.WriteLine($"El promedio total es: {promedio}");
+        }
+    }
+}
