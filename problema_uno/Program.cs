d8c7a818fe1415b8b92da849a9613841389ab92f
Gian <77172995+giansp@users.noreply.github.com>
2025-06-20 08:46:15 -0300

    Add files via upload

---
+++ problema_uno/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Apuestas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double apuesta, apuestaMenor, apuestaMayor;
+            string nombreApuesta, nombreApuestaMenor, nombreApuestaMayor;
+            bool stop = false;
+            apuestaMayor = 0;
+            apuestaMenor= 9999999999999999;
+            nombreApuestaMayor = "nn";
+            nombreApuestaMenor = "nn";
+
+            while (stop==false)
+            {
+                Console.WriteLine("Ingrese el nombre del apostador:");
+                nombreApuesta = Console.ReadLine();
+                Console.WriteLine("Ingrese el monto de la apuesta:");
+                apuesta = Convert.ToDouble(Console.ReadLine());
+                if (apuesta < 0)
+                {
+                    Console.WriteLine("El monto de la apuesta no puede ser negativo. Intente nuevamente.");
+                    continue;
+                }
+                if (apuesta == 0)
+                {
+                    Console.WriteLine("El monto de la apuesta no puede ser cero. Intente nuevamente.");
+                    continue;
+                }
+                if (apuesta<apuestaMenor)
+                    {
+                    apuestaMenor = apuesta;
+                    nombreApuestaMenor = nombreApuesta;
+                }
+                else
+                {
+                     if (apuesta>apuestaMayor)
+                    {
+                         apuestaMayor = apuesta;
+                        nombreApuestaMayor = nombreApuesta;
+                    }
+                }
+                Console.WriteLine("¿Desea continuar? (s/n):");
+                string respuesta = Console.ReadLine().ToLower();
+                if (respuesta!="s")
+                {
+                    stop = true;
+                    break;
+                }
+            }
+            Console.WriteLine("Apuesta menor: " + apuestaMenor + " por " + nombreApuestaMenor);
+            Console.WriteLine("Apuesta mayor: " + apuestaMayor + " por " + nombreApuestaMayor);
+        }
+    }
+}
